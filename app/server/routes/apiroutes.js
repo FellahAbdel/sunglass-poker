702e77537a38a918a562dee8850759563d6310ec
Victor-Gierlowski <59644630+Victor-Gierlowski@users.noreply.github.com>
2024-02-22 16:59:18 +0100

    Working WebServer & Api

--- app/server/routes/apiroutes.js
+++ app/server/routes/apiroutes.js
@@ -3,4 +3,14 @@ const router = express.Router();
 
 // Define your API routes here
 
+app = express();
+port = 3001;
 module.exports = router;
+
+app.get('/', (req,res) => {
+    res.send('welcome to my server');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
