e90a9da512568ea97b0ebfb7426b26acd8ed0f69
fellah <abdoulazizdiallofouta@gmail.com>
2024-02-23 10:46:13 +0100

    Signup base code

---
+++ app/web/src/components/signup/Signup.jsx
@@ -0,0 +1,10 @@
+// SignUp.js
+import React, { useState } from "react";
+import logo from "../logo.png";
+import "./SignUp.css"; // Import CSS file
+
+const SignUp = ({ showLogin, hideSignUp }) => {
+  // Component code
+};
+
+export default SignUp;
