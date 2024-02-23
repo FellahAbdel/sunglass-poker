7e5b66795c9e41b98c5922896e02ece94fa1f246
fellah <abdoulazizdiallofouta@gmail.com>
2024-02-23 10:45:38 +0100

    login base code

---
+++ app/web/src/components/login/Login.jsx
@@ -0,0 +1,10 @@
+// Login.js
+import React, { useState } from "react";
+import logo from "../logo.png";
+import "./Login.css"; // Import CSS file
+
+const Login = ({ showSignUp, hideLogin }) => {
+  // Component code
+};
+
+export default Login;
