79e27b991b1a976f1b05aca59b76ef3e32e139f6
DEMANGEON Noé <noe.demangeon@gmail.com>
2024-02-23 18:29:53 +0100

    Réorganisation code

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -1,21 +1,23 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/header/Header';
-import Home from './pages/home';
-import Login from './pages/login';
-import gameTable from './pages/gameTable';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/home";
+import gameTable from "./pages/gameTable";
+import { acceuil } from "./pages/login";
 
 function App() {
   return (
     <Router>
-      <div className="App"><Header/></div>
+      <div className="App">
+        <Header />
+      </div>
       <Routes>
-        <Route path="/home" Component={Home}/>
-        <Route path="/login" Component={Login}/>
-        <Route path="/gameTable" Component={gameTable}/>
+        <Route path="/home" Component={Home} />
+        <Route path="/login" Component={acceuil} />
+        <Route path="/gameTable" Component={gameTable} />
       </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
