f22cf8cb040c2203e15ad327e7587c766c67f32c
Heinrich Maël <mael.heinrich@etu.unistra.fr>
2024-02-25 20:47:57 +0100

    ajout js home et hearer,pb de css

--- app/web/src/pages/home.js
+++ app/web/src/pages/home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import "../components/header/home.css";
 
 export const home = () => { 
-    return (<div>
-        <div>Hello <h1>World</h1></div>
-    </div>
+    return (
+    <body>
+
+        <div>
+            <div>
+                <img src="../../../../../design/SunGameStudio_logo.png" alt="Logo de SunGamesStudio"></img>
+            </div>
+            <div>
+                <button><Link to="">JOUER</Link></button>
+                <button><Link to="">Code invite</Link></button>
+                <img src="../../../../../design/SunGlassPoker_logo.png" alt="Logo de SunGlassPoker"></img>
+            </div>
+        </div>
+    </body>
     )
 }
 
