72c928c7318e8172ff9e910d1fbbc90a2300f224
Mostafahqv Mac <mostafa.haghvirdiloo@etu.unistra.fr>
2024-02-26 18:03:39 +0100

    backdrop and basic table done !

--- app/web/src/components/gameTable/Table.js
+++ app/web/src/components/gameTable/Table.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styles from './Table.module.css'
 
-export class Table extends Component {
-  static propTypes = {}
-
-  render() {
-    return (
-      <div>Table</div>
-    )
-  }
+function Table() {
+  return (
+    <div className={`${styles.table}`}>
+        <div className={`${styles.cardsPlacement}`}>
+            <div className={`${styles.sunGlassText}`}>Sun Glass Poker</div>
+        </div>
+        <div className={`${styles.cardDuckPlacement}`}></div>
+    </div>
+  )
 }
 
-export default Table
+export default Table;
--- app/web/src/pages/gameTable.js
+++ app/web/src/pages/gameTable.js
@@ -2,12 +2,16 @@ import React from 'react';
 import styles from './gameTable.module.css';
 import Navbar from '../components/gameTable/Navbar';
 import BonusPanel from '../components/gameTable/BonusPanel';
+import Table from '../components/gameTable/Table';
 
 function gameTable() {
   return (
     <div className={`${styles.mainContainer}`}> 
+      <div className={`${styles.backdrop}`}></div>
       <div className={`${styles.Navbar}`}><Navbar/></div>
       <div className={`${styles.BonusPanel}`}><BonusPanel/></div>
+      <div className={`${styles.Table}`}><Table/></div>
+
     </div>
   );
 }
