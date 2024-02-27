187b23c593347c5aeb1778bd30b573e8cbac4c60
Victor-Gierlowski <59644630+Victor-Gierlowski@users.noreply.github.com>
2024-02-27 12:48:50 +0100

    wip socket.io server / client

--- app/server/routes/apiroutes.js
+++ app/server/routes/apiroutes.js
@@ -1,16 +1,29 @@
 const express = require('express');
-const router = express.Router();
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 3001; // Port du server
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Autorise les requêtes depuis n'importe quel domaine
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
 
-// Define your API routes here
-
-app = express();
-port = 3001; // Port du server
-module.exports = router;
-
-app.get('/', (req,res) => {
-    res.send('welcome to my server');
+app.get('/', (req, res) => {
+    res.send('');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+io.on('connection', (socket) => {
+    console.log('a user connected'); 
+    // io.on('hello', (data) => {
+    //     console.log('Received from client : ', data);
+    //     socket.emit('world', {responseData:'The world salute you'});
+    // });
+});
+
+
+server.listen(port, ()=> {
+    console.log('Server is running');
+});
--- app/web/src/App.js
+++ app/web/src/App.js
@@ -4,11 +4,18 @@ import Header from './components/header/Header';
 import Home from './pages/home';
 import Login from './pages/login';
 import gameTable from './pages/gameTable';
+import io from 'socket.io-client';
+const socket = io("<http://localhost:3001>");
 
+socket.emit('hello', {data: 'Hello world'});
+socket.on('world', (data) => {
+  console.log(data);
+})
 function App() {
   return (
     <Router>
-      <div className="App"><Header/></div>
+      <div className="App"><Header/>
+      </div>
       <Routes>
         <Route path="/home" Component={Home}/>
         <Route path="/login" Component={Login}/>
