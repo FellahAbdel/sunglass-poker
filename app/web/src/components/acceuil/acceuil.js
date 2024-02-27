f1cae7ceed18bc10ca0f4dc8066805343c2f4bab
Heinrich Maël <mael.heinrich@etu.unistra.fr>
2024-02-27 11:01:03 +0100

    modif css+onglet

--- app/web/src/components/acceuil/acceuil.js
+++ app/web/src/components/acceuil/acceuil.js
@@ -4,12 +4,12 @@ import "./acceuil.css";
 
 export const acceuil = () => { 
     return (
-        <div className='accueil'>
+        <div>
             <div className='accueil'>
                 <img src="../assets/SunGameStudio_logo.png" alt="Logo de SunGamesStudio"></img>
             </div>
             <div className='accueil'>
-                <button className='jouer'><Link to="">JOUER</Link></button>
+                <button className='jouer'><Link to="/login">JOUER</Link></button>
                 <button className='invite'><input type="text" placeholder="Code d'invitation"></input></button>
                 <img src="../../../../../design/SunGlassPoker_logo.png" alt="Logo de SunGlassPoker"></img>
             </div>
