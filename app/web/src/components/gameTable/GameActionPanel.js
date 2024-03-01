1efb810cfdb6f4774976e73bf97eb3ac0514f97a
Mostafahqv Mac <mostafa.haghvirdiloo@etu.unistra.fr>
2024-03-01 16:36:31 +0100

    animated

--- app/web/src/components/gameTable/GameActionPanel.js
+++ app/web/src/components/gameTable/GameActionPanel.js
@@ -2,6 +2,9 @@ import React , { useState } from 'react';
 import './gameActionPanel.css';
 import RaiseSlider from './RaiseSlider';
 
+//checkValue = true -> Check
+//checkValue = False -> Call
+//sliderValue text -> percentage of the raise
 const GameActionButtons = () => {
     const checkValue = useState(true);
     let checkOrCall = checkValue ? 'Check' : 'Call';
@@ -9,14 +12,22 @@ const GameActionButtons = () => {
     const [sliderValueText, setSliderValueText] = useState("");
     const handleSliderChange = (value) => {setSliderValueText(value)};
 
+
+    const [showPopup, setShowPopup] = useState(false);
+
+    const togglePopup = () => {
+        setShowPopup(!showPopup);
+    };
+    
+
   return (
     <div className="container-gameAction">
-    <div className="container-ActionButtons">
-          <button className="btn-raise">Raise {sliderValueText ? sliderValueText + "%" : sliderValueText}</button>
+    <div className={`container-ActionButtons ${showPopup ? "container-ActionButtons-slideUp" : ""}`}>
+          <button className="btn-raise" onClick={togglePopup} >Raise {sliderValueText ? sliderValueText + "%" : ""}</button>
           <button className="btn-checkOrCall">{checkOrCall}</button>
           <button className="btn-fold">Fold</button>
       </div>
-      <RaiseSlider  initialValue={25} onSliderChange={handleSliderChange}/>
+      <div className={`rangeSlider ${showPopup ? "rangeSlider-open" : ""}`}><RaiseSlider initialValue={25} onSliderChange={handleSliderChange}/></div>
     </div>
   )
 }
