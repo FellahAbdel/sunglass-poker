b30088594d254f15299997d2a528a2d869b2fa0b
Victor-Gierlowski <59644630+Victor-Gierlowski@users.noreply.github.com>
2024-03-08 16:37:35 +0100

    :memo: Ajout de commentaires

--- app/server/routes/apiroutes.js
+++ app/server/routes/apiroutes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+
+/** Paramètres cors du serveur.
+ * 
+ * origin -> Fixer sur le front-end. Il n'y a que le serveur front end avec qui la communication est autoriser.
+ *  */
 const io = require('socket.io')(server, {
     cors: {
         origin: "http://localhost:3000",
@@ -9,12 +14,24 @@ const io = require('socket.io')(server, {
         credentials: true
       }
 });
-const port = 3001; // Port du server
 
+// Port du server
+const port = 3001;
+
+// Requête vide on n'envoit rien.
 app.get('/', (req, res) => {
     res.send('');
 });
 
+
+/** Lorsqu'une page est ouverte et se connecte au back.
+ * 
+ *  On log la session et son Id.
+ * 
+ *  La requête 'Hello' est une requête de test d'échange. On log la data et en renvoit. le front log aussi la data reçu.
+ * 
+ *  En cas de déconnexion on log la session Id qui s'arrête.
+ */
 io.on('connection', (socket) => {
     console.log('a user connected n:'+socket.id); 
     socket.on('hello', (data) => {
@@ -27,6 +44,9 @@ io.on('connection', (socket) => {
 });
 
 
+/** Démarrage du serveur.
+ * 
+ */
 server.listen(port, ()=> {
     console.log('Server is running on port ' + port);
 });
