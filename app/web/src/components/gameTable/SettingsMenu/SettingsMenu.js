2b90ddd9e4f94ed52702161d41918036b1f2d6d5
Mostafahqv Mac <mostafa.haghvirdiloo@etu.unistra.fr>
2024-03-12 16:54:03 +0100

    night mode toggle

--- app/web/src/components/gameTable/SettingsMenu/SettingsMenu.js
+++ app/web/src/components/gameTable/SettingsMenu/SettingsMenu.js
@@ -2,7 +2,7 @@ import React ,{useState} from 'react';
 import './settingsMenu.css'
 import Button from '../Button/Button.tsx';
 
-const settingsMenu = ({handleSliderChange,checkBox}) => {
+const settingsMenu = ({handleSliderChange,checkBox,nightMode}) => {
 
   return (
     <div className="settingsMenu">
@@ -12,14 +12,17 @@ const settingsMenu = ({handleSliderChange,checkBox}) => {
             <input type="checkbox" value={checkBox}/>
             Mute
           </label>
+
+          NIGHT MODE
+          <label class="switch">
+            <input type="checkbox" onClick={nightMode}/>
+            <span class="slider"></span>
+          </label>
+
           <div className='container-ranges'>
           <div className='rangeContainer-blah'>
             BLAH BLAH   
-            <input type='range' min={1} max={100}  step={1} onChange={handleSliderChange}></input>
-            </div>
-            <div className='rangeContainer-blah'>
-            BLAH BLAH   
-            <input className='range-settings' type='range' min={1} max={100}  step={1} onChange={handleSliderChange}/>
+            <input className='slider-blah' type='range' min={1} max={100}  step={1} onChange={handleSliderChange}/>
             </div>
           </div>
           
