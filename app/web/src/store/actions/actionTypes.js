0f38645cd6f7160d92f9e3cd0033084cb88dc63b
fellah <abdoulazizdiallofouta@gmail.com>
2024-03-12 13:54:11 +0100

    Add all possible action types

---
+++ app/web/src/store/actions/actionTypes.js
@@ -0,0 +1,8 @@
+export const DEAL_CARDS = "DEAL_CARDS";
+export const BET = "BET";
+export const FOLD = "FOLD";
+export const CHECK = "CHECK";
+export const CALL = "CALL";
+export const RAISE = "RAISE";
+export const END_ROUND = "END_ROUND";
+export const RESET_GAME = "RESET_GAME";
