a4d4f7aacf1e3befcfade892457df598d04ac95a
fellah <abdoulazizdiallofouta@gmail.com>
2024-03-13 12:09:54 +0100

    move the route above + code formated

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -2,7 +2,7 @@
 // import React, { useState } from 'react';
 // import './App.css';
 // import { BrowserRouter as Router, Route, Routes }from 'react-router-dom';
-import GameTable from './pages/gameTable';
+import GameTable from "./pages/gameTable";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
@@ -11,8 +11,6 @@ import Acceuil from "./components/acceuil/Acceuil";
 import { AuthProvider } from "./components/AuthProvider";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-
-
 
 function App() {
   const [isWindowOpen, setIsWindowOpen] = useState(false);
@@ -29,18 +27,16 @@ function App() {
 
   return (
     <Router>
-          <Routes>
-        <Route path="/GameTable" Component={GameTable}/>
-      </Routes>
       <AuthProvider>
-
-
         <Header
           openWindow={openWindow}
           closeWindow={closeWindow}
           isWindowOpen={isWindowOpen}
           windowType={windowType}
         />
+        <Routes>
+          <Route path="/GameTable" Component={GameTable} />
+        </Routes>
 
         {/* <Acceuil openWindow={openWindow} /> */}
       </AuthProvider>
