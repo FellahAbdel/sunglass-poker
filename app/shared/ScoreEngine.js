663bb0d04f7d3c6e9262b1f77b00625b48cad637
fellah <abdoulazizdiallofouta@gmail.com>
2024-03-15 00:07:03 +0100

    class base to update

---
+++ app/shared/ScoreEngine.js
@@ -0,0 +1,19 @@
+// ScoreEngine.js
+
+class ScoreEngine {
+  constructor() {}
+
+  estDoublePaire(cards) {
+    // Code for estDoublePaire function
+  }
+
+  estPaire(cards) {
+    // Code for estPaire function
+  }
+
+  estCarteHaute(cards) {
+    // Code for estCarteHaute function
+  }
+}
+
+module.exports = ScoreEngine;
