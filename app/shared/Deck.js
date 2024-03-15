bc03830eaf1e0df1de26593b21babc15bc247655
fellah <abdoulazizdiallofouta@gmail.com>
2024-03-15 12:01:25 +0100

    value instead of string

--- app/shared/Deck.js
+++ app/shared/Deck.js
@@ -13,21 +13,7 @@ class Deck {
   reset() {
     this.cards = [];
     const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
-    const values = [
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K",
-      "A",
-    ];
+    const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 1];
 
     for (let suit of suits) {
       for (let value of values) {
