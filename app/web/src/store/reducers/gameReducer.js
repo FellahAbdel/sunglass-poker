53a4fd6ea2c01ae2c9a6b5869083ead3b17a0a88
Heinrich Maël <mael.heinrich@etu.unistra.fr>
2024-03-25 18:25:05 +0100

    :sparkles: ajout du redux

--- app/web/src/store/reducers/gameReducer.js
+++ app/web/src/store/reducers/gameReducer.js
@@ -7,8 +7,12 @@ const initialState = {
     // Initial state of user
     chips: 1000,
     // User's available chips
-    betAmount: 0
+    betAmount: 0,
     // User's current bet amount
+
+    //User card
+    cards:[]
+
   }
   // Other game state variables can be added here
 };
@@ -24,7 +28,27 @@ const gameReducer = (state = initialState, action) => {
           chips: state.user.chips - action.payload,
           betAmount: state.user.betAmount + action.payload
         }
+        
       };
+    case DISTRIBUTE_CARDS:
+      // Logique pour distribuer les cartes
+      return {
+        ...state,
+        cards: state.user.cards + action.payload
+      };
+    case MISER:
+      // Logique pour distribuer les cartes
+      return {
+        ...state,
+        betAmount: state.user.betAmount + action.payload
+      };
+    case WIN:
+      // Logique pour distribuer les cartes
+      return {
+        ...state.user,
+        chips: state.user.chips + action.payload, //en gros on ajoute le pot
+      };
+
     // Other game actions can be handled here
     default:
       return state;
