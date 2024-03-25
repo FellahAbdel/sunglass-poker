fb13baf78c51c353532ae3ec9b83eb0aa116095e
fellah <abdoulazizdiallofouta@gmail.com>
2024-03-25 18:09:29 +0100

    pack and import functions

---
+++ app/shared/score-engine/index.js
@@ -0,0 +1,23 @@
+const flush = require("./flush");
+const fourOfAKind = require("./fourOfAKind");
+const fullHouse = require("./fullHouse");
+const highCard = require("./highCard");
+const onePair = require("./onePair");
+const royalFlush = require("./royalFlush");
+const straight = require("./straight");
+const straightFlush = require("./straightFlush");
+const threeOfAKind = require("./threeOfAKind");
+const twoPair = require("./twoPair");
+
+module.exports = {
+  flush,
+  fourOfAKind,
+  fullHouse,
+  highCard,
+  onePair,
+  royalFlush,
+  straight,
+  straightFlush,
+  threeOfAKind,
+  twoPair,
+};
