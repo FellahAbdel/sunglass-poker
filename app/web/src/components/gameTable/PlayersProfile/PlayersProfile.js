921163f6352a67172813fd0780e3f4beeae1c790
fellah <abdoulazizdiallofouta@gmail.com>
2024-03-27 15:07:56 +0100

    format playersProfile

--- app/web/src/components/gameTable/PlayersProfile/PlayersProfile.js
+++ app/web/src/components/gameTable/PlayersProfile/PlayersProfile.js
@@ -1,38 +1,62 @@
-import React from 'react';
-import './playersProfile.css';
-import ProgressBar from '../../Utiles/ProgressBar';
-import Card from '../Card/Card';
+import React from "react";
+import "./playersProfile.css";
+import ProgressBar from "../../Utiles/ProgressBar";
+import Card from "../Card/Card";
 
-const  PlayersProfile=({status, chips, name , cards , flippingPlayerCards , gotCards}) =>{
-    const formattedChips = chips.toLocaleString();
-    const dollarSign = '$';
+const PlayersProfile = ({
+  status,
+  chips,
+  name,
+  cards,
+  flippingPlayerCards,
+  gotCards,
+}) => {
+  const formattedChips = chips.toLocaleString();
+  const dollarSign = "$";
 
-
-    return (
+  return (
     <div className="container-onGameProfile">
-        <div className={`box-status  ${status}`}>
-
+      <div className={`box-status  ${status}`}>
         {status === "Waiting" ? (
-                <ProgressBar className="progressBar progressPercentage" durationInSeconds={5} />
-            ) : (
-                <div className={`box-statusText  ${status}`}>{status}</div>
-            )}
-        </div>
+          <ProgressBar
+            className="progressBar progressPercentage"
+            durationInSeconds={5}
+          />
+        ) : (
+          <div className={`box-statusText  ${status}`}>{status}</div>
+        )}
+      </div>
 
-        <div className={`box-playerInfo ${status}`}>
-            {formattedChips} {status === "Empty" ? null : dollarSign}<br/>
-            {name}
-        </div>
-        
+      <div className={`box-playerInfo ${status}`}>
+        {formattedChips} {status === "Empty" ? null : dollarSign}
+        <br />
+        {name}
+      </div>
 
-        <Card style={`showCardPlayers1 ${gotCards ? "playerCardAppear" : "" }`} card={["a","clubs"]} flippedStyle={"flippedPlayerCards"} flippingCard={flippingPlayerCards}/>
-        <Card style={`showCardPlayers2 ${gotCards ? "playerCardAppear" : "" }`} card={["a","clubs"]} flippedStyle={"flippedPlayerCards"} flippingCard={flippingPlayerCards}/>
+      <Card
+        style={`showCardPlayers1 ${gotCards ? "playerCardAppear" : ""}`}
+        card={["a", "clubs"]}
+        flippedStyle={"flippedPlayerCards"}
+        flippingCard={flippingPlayerCards}
+      />
+      <Card
+        style={`showCardPlayers2 ${gotCards ? "playerCardAppear" : ""}`}
+        card={["a", "clubs"]}
+        flippedStyle={"flippedPlayerCards"}
+        flippingCard={flippingPlayerCards}
+      />
 
-
-        <img className={`profilePic ${status}`} src={status === "Empty" ? require('./../../assets/images/pp_empty.png') : require('./../../assets/images/pp_simple.jpg')} alt='profilePic'/>
-        
+      <img
+        className={`profilePic ${status}`}
+        src={
+          status === "Empty"
+            ? require("./../../assets/images/pp_empty.png")
+            : require("./../../assets/images/pp_simple.jpg")
+        }
+        alt="profilePic"
+      />
     </div>
-    )
-}
+  );
+};
 
 export default PlayersProfile;
