b9f8a4385d6001996f8ec3b0c7877a210ffd3e9a
fellah <abdoulazizdiallofouta@gmail.com>
2024-03-27 15:11:27 +0100

    format the card component

--- app/web/src/components/gameTable/Card/Card.js
+++ app/web/src/components/gameTable/Card/Card.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import './card.css';
-import cardBack from './../../assets/images/card-design.png';
+import React from "react";
+import "./card.css";
+import cardBack from "./../../assets/images/card-design.png";
 
-
-const Card = ({card,style,flippingCard, flippedStyle}) => {
+const Card = ({ card, style, flippingCard, flippedStyle }) => {
   return (
-    <div className={`${style} ${flippingCard ? `flippedCard ${flippedStyle}` : ""}`}>
-        <img className="CardBack" src={cardBack} alt="card"/> 
-        {card ? <img className="CardFront" src={require(`./../../assets/images/card_front/${card[0]}_of_${card[1]}.png`)} alt="card"/> : null}
+    <div
+      className={`${style} ${
+        flippingCard ? `flippedCard ${flippedStyle}` : ""
+      }`}
+    >
+      <img className="CardBack" src={cardBack} alt="card" />
+      {card ? (
+        <img
+          className="CardFront"
+          src={require(`./../../assets/images/card_front/${card[0]}_of_${card[1]}.png`)}
+          alt="card"
+        />
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
 export default Card;
