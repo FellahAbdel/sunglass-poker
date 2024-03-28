3910c5414872e29a8a08ea7e86300b49da5fd7bd
fellah <abdoulazizdiallofouta@gmail.com>
2024-03-28 21:32:55 +0100

    move provider inside the app in index.js

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -1,7 +1,5 @@
 // import { useAuth, getUserInfo } from "../AuthProvider";
 // import React, { useState } from 'react';
-import store from "./store/configureStore";
-import { Provider } from "react-redux";
 import "./App.css";
 // import { BrowserRouter as Router, Route, Routes }from 'react-router-dom';
 import GameTable from "./pages/gameTable";
@@ -16,19 +14,17 @@ import { SettingsProvider } from "./components/Utiles/SettingsContext";
 
 function App() {
   return (
-    <Provider store={store}>
-      <WindowProvider>
-        <Router>
-          <AuthProvider>
-            <SettingsProvider>
-              <Routes>
-                <Route path="/" Component={GameTable} />
-              </Routes>
-            </SettingsProvider>
-          </AuthProvider>
-        </Router>
-      </WindowProvider>
-    </Provider>
+    <WindowProvider>
+      <Router>
+        <AuthProvider>
+          <SettingsProvider>
+            <Routes>
+              <Route path="/" Component={GameTable} />
+            </Routes>
+          </SettingsProvider>
+        </AuthProvider>
+      </Router>
+    </WindowProvider>
   );
 }
 
