3edca55e09fbc3c8d182844b7cfeaf49206a190c
fellah <abdoulazizdiallofouta@gmail.com>
2024-04-02 12:20:30 +0200

    small and big blind

--- app/web/src/store/reducers/gameReducer.js
+++ app/web/src/store/reducers/gameReducer.js
@@ -4,6 +4,8 @@ import Deck from "../../shared/Deck.js";
 import Player from "../../shared/Player.js";
 import Game from "../../shared/Game.js";
 
+// import { useUserData } from "../../Utiles/useUserData";
+
 const initialState = {
   gameClass: new Game(),
   // Initialy the game is in the preGame state and players can join
@@ -23,10 +25,33 @@ const begin = (gameState) => {
   const players = gameState.players;
   players.forEach((player) => gameState.gameClass.addPlayer(player));
 
+  // Distribute small and big blinds
+  const smallBlindPlayerId = 0; /* logic to determine small blind player id */
+  const bigBlindPlayerId = 1; /* logic to determine big blind player id */
+  const smallBlindAmount = 10; /* your small blind amount */
+  const bigBlindAmount = 20; /* your big blind amount */
+
+  // Designate small and big blinds
+  gameState.gameClass.smallBlindPlayerId = smallBlindPlayerId;
+  gameState.gameClass.bigBlindPlayerId = bigBlindPlayerId;
+
+  // Distribute blinds
+  gameState.gameClass.pokerTable.playerBet(
+    gameState.gameClass.players[smallBlindPlayerId],
+    smallBlindAmount
+  );
+  gameState.gameClass.pokerTable.playerBet(
+    gameState.gameClass.players[bigBlindPlayerId],
+    bigBlindAmount
+  );
+
   // Start the game
   gameState.gameClass.start();
 
-  return gameState;
+  return {
+    ...gameState,
+    controlsMode: "roundOne",
+  };
 };
 
 const gameReducer = (state = initialState, action) => {
@@ -36,7 +61,6 @@ const gameReducer = (state = initialState, action) => {
         ...state,
         players: [...state.players, action.payload], // Add the new player to the list
       };
-
     case actions.START_GAME:
       return begin(state);
     // Other game actions can be handled here
