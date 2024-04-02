74f478df14b93e57353c51cc194c829e8ed62acf
fellah <abdoulazizdiallofouta@gmail.com>
2024-04-03 01:57:45 +0200

    new config by using socketMiddleware

--- app/web/src/store/configureStore.js
+++ app/web/src/store/configureStore.js
@@ -1,13 +1,17 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
-
 import logger from "redux-logger";
-
 import rootReducer from "./reducers/rootReducer.js";
+import { socketMiddleware } from "./middleware/socketMiddleware"; // Adjust the path accordingly
+import io from "socket.io-client";
 
+// Create Socket.io instance
+const socket = io("http://localhost:3001"); // Adjust the URL accordingly
+
+// Apply middleware when creating the Redux store
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(logger, socketMiddleware(socket)))
 );
 
 export default store;
