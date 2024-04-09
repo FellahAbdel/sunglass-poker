bbe22633f67a3a0ec103eeff10b48ce5b1483411
Heinrich Maël <mael.heinrich@etu.unistra.fr>
2024-04-09 10:56:11 +0200

    ajout message d'erreur(marche pas)

--- app/web/electron-main.js
+++ app/web/electron-main.js
@@ -8,25 +8,41 @@ const { create } = require("domain");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width:1920,
-    height:1080,
-    webPreferences:{
-      nodeIntegration: true
+    width: 1920,
+    height: 1080,
+    webPreferences: {
+      nodeIntegration: true,
+      devTools: false,
+      enableRemoteModule: false,
+      webSecurity: true
     }
-  })
-  win.setMenu(null);
-
-  // Gérer les événements d'erreur de chargement de la page
-  win.webContents.on("did-fail-load", (event, errorCode, errorDescription, validatedURL, isMainFrame) => {
-    // Afficher un message d'erreur à l'utilisateur
-    dialog.showErrorBox("Erreur de chargement de la page", `Code d'erreur: ${errorCode}\nDescription de l'erreur: ${errorDescription}`);
   });
 
-  win.loadURL("http://localhost:3001")
-  //win.loadURL("https://httpstat.us/404");
-  //win.loadURL("http://example.com/non-existent");
+  win.setMenu(null);
 
+  win.webContents.on("did-fail-load", (event, errorCode, errorDescription, validatedURL, isMainFrame) => {
+    if (errorCode === -3) { // Vérifier si c'est une erreur JavaScript
+      // Afficher une boîte de dialogue personnalisée avec un bouton de réessai
+      const options = {
+        type: "error",
+        title: "Erreur JavaScript",
+        message: "Une erreur JavaScript est survenue. Voulez-vous réessayer ?",
+        buttons: ["Réessayer", "Annuler"]
+      };
 
+      dialog.showMessageBox(win, options).then((response) => {
+        if (response.response === 0) { // Si le bouton "Réessayer" est cliqué
+          win.reload(); // Recharger la page
+        }
+      });
+    } else {
+      // Gérer les autres types d'erreurs de chargement de page si nécessaire
+    }
+  });
+
+  // Charger l'URL initiale
+  win.loadURL("http://localhost:3000");
 }
+
 
 app.whenReady().then(createWindow);
