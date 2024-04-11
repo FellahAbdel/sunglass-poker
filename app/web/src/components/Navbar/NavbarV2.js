e01ed44ab0b45b11d24a4f21f874da4b5897b830
DEMANGEON Noé <noe.demangeon@gmail.com>
2024-04-11 15:01:45 +0200

    Bouton retour

--- app/web/src/components/Navbar/NavbarV2.js
+++ app/web/src/components/Navbar/NavbarV2.js
@@ -12,16 +12,17 @@ import { useAuth } from "../Utiles/AuthProvider";
 import { useUserData } from "../Utiles/useUserData";
 import { useWindowContext } from "../Utiles/WindowContext.jsx";
 
-
 const Navbar = ({}) => {
-  const { user, isLogged,logingOut } = useAuth();
-  const {isGameTableVisible,closeWindow,openWindow} = useWindowContext();
+  const { user, isLogged, logingOut } = useAuth();
+  const { isGameTableVisible, closeWindow, openWindow, showHome } = useWindowContext();
   const userData = useUserData();
 
-
   const handleLogOutButton = () => {
-    logingOut();
-    closeWindow();
+    if (isGameTableVisible) {
+      showHome();
+    } else {
+      logingOut();
+    }
   };
 
   const handleClick = (e) => {
@@ -47,41 +48,42 @@ const Navbar = ({}) => {
       {isLogged && (
         <>
           <div className={`chatBox-V2 ${isChatOpen && "chatBoxOpen-V2"}`}>
-            {isGameTableVisible && <>
-            {!isChatOpen && (
-              <Button
-                label={""}
-                onClick={handleChatOpen}
-                iconSrc={require("./../assets/images/icons/white/chat.png")}
-              />
-            )}
-
-            {isChatOpen && (
+            {isGameTableVisible && (
               <>
-                <img
-                  className={"btn-chatClose-V2"}
-                  onClick={handleChatClose}
-                  src={require("./../assets/images/icons/white/cross.png")}
-                  alt="exit-chat"
-                />
-                <TextInputComponent
-                  name="Message"
-                  value={handleNull}
-                  onChange={handleNull}
-                  placeholder={"Messages"}
-                  styleClass={"input-chatBox-V2"}
-                />
+                {!isChatOpen && (
+                  <Button
+                    label={""}
+                    onClick={handleChatOpen}
+                    iconSrc={require("./../assets/images/icons/white/chat.png")}
+                  />
+                )}
+
+                {isChatOpen && (
+                  <>
+                    <img
+                      className={"btn-chatClose-V2"}
+                      onClick={handleChatClose}
+                      src={require("./../assets/images/icons/white/cross.png")}
+                      alt="exit-chat"
+                    />
+                    <TextInputComponent
+                      name="Message"
+                      value={handleNull}
+                      onChange={handleNull}
+                      placeholder={"Messages"}
+                      styleClass={"input-chatBox-V2"}
+                    />
+                  </>
+                )}
               </>
-              )}
-            </>}
-            </div>
-        
+            )}
+          </div>
+
           {/* <ChipsCash
             // currentChips={user?.coins}
             currentChips={99999999}
             styleClass={`box-chips-V2 `}
           /> */}
-
         </>
       )}
 
@@ -90,44 +92,42 @@ const Navbar = ({}) => {
       */}
 
       <div className="container-navMain-V2">
-        {isLogged &&
-        <Button
-          label={isLogged
-            ? getTranslatedWord("navbar.profile") 
-            : getTranslatedWord("navbar.login")}
-          onClick={() => (isLogged ? openWindow("profile") :openWindow("login"))}
-          styleClass={`${
-            isLogged ? "btn-profile-V2 " : "btn-logIn-V2 "
-          }`}
-          iconSrc={require("./../assets/images/icons/white/profile.png")}
-        />}
-
+        {isLogged && (
+          <Button
+            label={
+              isLogged
+                ? getTranslatedWord("navbar.profile")
+                : getTranslatedWord("navbar.login")
+            }
+            onClick={() =>
+              isLogged ? openWindow("profile") : openWindow("login")
+            }
+            styleClass={`${isLogged ? "btn-profile-V2 " : "btn-logIn-V2 "}`}
+            iconSrc={require("./../assets/images/icons/white/profile.png")}
+          />
+        )}
 
         <Button
           label={getTranslatedWord("navbar.settings")}
           onClick={() => openWindow("settings")}
-          styleClass={`${
-            isLogged ? "btn-settings-V2 " : "btn-settings-V2 "
-          }`}
+          styleClass={`${isLogged ? "btn-settings-V2 " : "btn-settings-V2 "}`}
           iconSrc={require("./../assets/images/icons/white/settings.png")}
         />
 
         <Button
           label={getTranslatedWord("navbar.tutorial")}
           onClick={() => openWindow("tutorial")}
-          styleClass={`${
-            isLogged ? "btn-tutorial-V2 " : "btn-tutorial-V2 "
-          }`}
+          styleClass={`${isLogged ? "btn-tutorial-V2 " : "btn-tutorial-V2 "}`}
           iconSrc={require("./../assets/images/icons/white/tutorial.png")}
         />
-        {isLogged && 
-        <Button
-          label={getTranslatedWord("navbar.exit")}
-          onClick={handleLogOutButton}
-          styleClass="btn-exit-V2"
-          iconSrc={require("./../assets/images/icons/white/exit.png")}
-        />
-      }
+        {isLogged && (
+          <Button
+            label={getTranslatedWord("navbar.exit")}
+            onClick={handleLogOutButton}
+            styleClass="btn-exit-V2"
+            iconSrc={require("./../assets/images/icons/white/exit.png")}
+          />
+        )}
       </div>
     </div>
   );
