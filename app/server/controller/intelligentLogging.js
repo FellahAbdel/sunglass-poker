09b5ecb9ac08b51214bb53d7f8f0ae839af58787
Victor-Gierlowski <59644630+Victor-Gierlowski@users.noreply.github.com>
2024-04-14 16:38:30 +0200

    🎨 3.0 intelligentLogging
    Sync the console and the log, force log  to be in correct order.
    Code structure simplify.

--- app/server/controller/intelligentLogging.js
+++ app/server/controller/intelligentLogging.js
@@ -4,18 +4,18 @@ module.exports = csl = {
     outputFile: false,
     silenced: [],
     outLocaly: false,
-    outLocal: function (type, out, ...args) {
-        var element = "<div class='" + out + "'><h2>" + type + "</h2></br><section>";
-        for (i in args) {
-            var subElement = "<span>";
-            data = args[i];
-            if (typeof data === 'object')
-                subElement += '<pre>' +this.getObjString(data) + '</pre>';
-            else subElement += data;
-            subElement += "</span>";
-            element += subElement;
-        }
-        element += "</section></div>";
+    buffLocal: [],
+    buffCallLocal: [],
+    buffConsole: [],
+    outLocalAdd: function(args) {
+        this.buffCallLocal.push(args);
+    },
+    callLocal: function(){
+        args = this.buffCallLocal.shift();
+        this.outLocal.apply(this,args);
+    },
+    writeLocal: async function(){
+        const element = this.buffLocal.shift();
         if (this.outputFile === false) {
             // this.outputFile = './log_'+Math.random()+'.html';
             this.outputFile = './log.html';
@@ -24,6 +24,28 @@ module.exports = csl = {
         fs.appendFile(this.outputFile, element, err => {
             if (err) { console.error('cannot output locally logs', err); }
         });
+    },
+    buffConsoleAdd: function(args) {
+        this.buffConsole.push(args);
+    },
+    writeConsole: async function(){
+        args = this.buffConsole.shift();
+        this.pretty.apply(this,args);
+    },
+    outLocal: function (type, out, ...args) {
+        var element = "<div class='" + out + ' ' + type + "'><h2>" + type + "</h2></br><section>";
+        for (i in args) {
+            var subElement = "<span>";
+            data = args[i];
+            if (typeof data === 'object')
+                subElement += '<pre>' + this.getObjString(data) + '</pre>';
+            else subElement += data;
+            subElement += "</span>";
+            element += subElement;
+        }
+        element += "</section></div>";
+        this.buffLocal.push(element);
+        this.writeLocal();
     },
     getObjString: function (obj, decorate = '    ') {
         var output = ' { ';
@@ -60,26 +82,24 @@ module.exports = csl = {
         }
         console.log("\n------------ [-" + type + "-]\n");
     },
-    log: function (type, ...data) {
-        if (this.outLocaly)
-            this.outLocal(type, 'log', ...data);
-        if (!this.silenced[type]) {
-            this.pretty(type, 'log', ...data);
+    prelog: function(type,out, ...data){
+        if (this.outLocaly){
+            this.outLocalAdd([type, out, ...data]);
+            this.callLocal();
         }
+        if (!this.silenced[type]) {
+            this.buffConsoleAdd([type, out, ...data]);
+            this.writeConsole();
+        }
+    },
+    log: function (type, ...data) {
+        this.prelog(type,'log',...data);
     },
     table: function (type, ...data) {
-        if (this.outLocaly)
-            this.outLocal(type, 'table', ...data);
-        if (!this.silenced[type]) {
-            this.pretty(type, 'table', ...data);
-        }
+        this.prelog(type,'table',...data);
     },
     error: function (type, ...data) {
-        if (this.outLocaly)
-            this.outLocal(type, 'error', ...data);
-        if (!this.silenced[type]) {
-            this.pretty(type, 'error', ...data);
-        }
+        this.prelog(type,'error',...data);
     },
     silenced: function (type) {
         if (Array.isArray(type)) {
