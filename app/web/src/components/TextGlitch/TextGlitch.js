6dfd0a7cbb49c27c034cc8ec11dee8a588786dd9
Mostafahqv Mac <mostafa.haghvirdiloo@etu.unistra.fr>
2024-04-16 12:50:48 +0200

    glitch animation turns off , logo css fix

--- app/web/src/components/TextGlitch/TextGlitch.js
+++ app/web/src/components/TextGlitch/TextGlitch.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./textGlitch.css";
+import { useSettings } from "./../Utiles/SettingsContext.jsx";
 
-function textGlitch({ children, styleClass, glitchStyle }) {
+function TextGlitch({ children, styleClass, glitchStyle }) {
+  const { animation } = useSettings();
+
   return (
     <div className={styleClass}>
-      <div className={`glitch-wrapper`}>
-        <div className={`glitch ${glitchStyle}`} data-glitch={children}>
+      <div className={`${animation ? "glitch-wrapper" : ""}`}>
+        <div className={`${animation ? `glitch ${glitchStyle}` : "noAnimationGlitch" }`} data-glitch={children}>
           {children}
         </div>
       </div>
@@ -13,4 +16,4 @@ function textGlitch({ children, styleClass, glitchStyle }) {
   );
 }
 
-export default textGlitch;
+export default TextGlitch;
