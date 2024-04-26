db5b6c0737a60eeaacc2389fe6a36d81648bb2b3
fellah <abdoulazizdiallofouta@gmail.com>
2024-04-26 07:26:22 +0200

    Money on table are dynamic but there is some logic issue instead of having 120 after the small and big blind we got 60, and  when we start another party the same counter is incremented

--- app/web/src/components/Table/PlayersPots.js
+++ app/web/src/components/Table/PlayersPots.js
@@ -1,27 +1,49 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useTranslation } from "../Utiles/Translations";
 import "./table.css";
 
-const PlayersPots = ({testOnClick1,testOnClick2,testOnClick3}) => {
-  const playersInTable = useSelector((state) => state.game.players);
-  const [moneyPot,setMoneyPot] = useState(9999999);
+const PlayersPots = ({ testOnClick1, testOnClick2, testOnClick3 }) => {
+  // le premier game est le nom du reducer dans le store
+  // le deuxieme game est le nom de la class game dans shared.
+
+  const gameClass = useSelector((state) => state.game.game);
+  
+  // Définir currentStack avec une valeur par défaut de 0
+  const currentStack = gameClass?.pokerTable?.stack || 0;
+
+  // Définir updatedStack à partir de currentStack
+  const [updatedStack, setUpdatedStack] = useState(currentStack);
+
+  useEffect(() => {
+    // Mettre à jour updatedStack si currentStack change
+    setUpdatedStack(currentStack);
+  }, [currentStack]);
+
   const { getTranslatedWord } = useTranslation();
 
   return (
     <div className="container-playerPots">
-        <div className={`container-totalPot`}>{getTranslatedWord("table.total")}: {moneyPot.toLocaleString()} SC</div>
+      <div className={`container-totalPot`}>
+        {getTranslatedWord("table.total")}: {updatedStack.toLocaleString()} SC
+      </div>
 
-        <div className="playerPot0" onClick={testOnClick1}>1000 SC</div>
-        <div className="playerPot1" onClick={testOnClick2}>1000 SC</div>
-        <div className="playerPot2" onClick={testOnClick3}>1000 SC</div>
-        <div className="playerPot3">1100 SC</div>
-        <div className="playerPot4">1100 SC</div>
-        <div className="playerPot5">1000 SC</div>
-        <div className="playerPot6">900 SC</div>
-        {/* <div className="playerPot7">1000 SC</div> */}
-        <div className="playerPot8">1000 SC</div>
-        <div className="playerPot9">1000 SC</div>
+      <div className="playerPot0" onClick={testOnClick1}>
+        1000 SC
+      </div>
+      <div className="playerPot1" onClick={testOnClick2}>
+        1000 SC
+      </div>
+      <div className="playerPot2" onClick={testOnClick3}>
+        1000 SC
+      </div>
+      <div className="playerPot3">1100 SC</div>
+      <div className="playerPot4">1100 SC</div>
+      <div className="playerPot5">1000 SC</div>
+      <div className="playerPot6">900 SC</div>
+      {/* <div className="playerPot7">1000 SC</div> */}
+      <div className="playerPot8">1000 SC</div>
+      <div className="playerPot9">1000 SC</div>
     </div>
   );
 };
