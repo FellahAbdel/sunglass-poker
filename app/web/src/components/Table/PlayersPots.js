0e66ae3a2a1551905e11f4c47b43188ff13a6242
fellah <abdoulazizdiallofouta@gmail.com>
2024-04-28 01:37:37 +0200

    empty players in params

--- app/web/src/components/Table/PlayersPots.js
+++ app/web/src/components/Table/PlayersPots.js
@@ -3,29 +3,7 @@ import { useSelector } from "react-redux";
 import { useTranslation } from "../Utiles/Translations";
 import "./table.css";
 
-const PlayersPots = ({ players = [], stack = 0 }) => {
-  // le premier game est le nom du reducer dans le store
-  // le deuxieme game est le nom de la class game dans shared.
-    console.log("playerls in players pots", players);
-    console.log("stack in players pots", stack);
-
-    const localStack= 0;
-//   const gameClass = useSelector((state) => state.game.game);
-
-//   // Définir currentStack avec une valeur par défaut de 0
-//   const currentStack = gameClass?.pokerTable?.stack || 0;
-
-//   // Définir updatedStack à partir de currentStack
-//   const [updatedStack, setUpdatedStack] = useState(currentStack);
-
-//   useEffect(() => {
-//     // Mettre à jour updatedStack si currentStack change
-//     setUpdatedStack(currentStack);
-//   }, [currentStack]);
-
-  const { getTranslatedWord } = useTranslation();
-  
-
+const PlayersPots = ({ players = []}) => {
   return (
     <div className="container-playerPots">
       {players.map((player, index) => (
