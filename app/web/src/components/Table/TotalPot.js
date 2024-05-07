2b354efb1c5fde227326a347fb397de4d09f8d3c
fellah <abdoulazizdiallofouta@gmail.com>
2024-05-07 14:41:37 +0200

    no need anymore

--- app/web/src/components/Table/TotalPot.js
+++ app/web/src/components/Table/TotalPot.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import React from "react";
 import { useTranslation } from "../Utiles/Translations";
 import "./table.css";
 import { useGameTable } from "../Utiles/GameTableProvider";
@@ -7,19 +6,6 @@ import { useGameTable } from "../Utiles/GameTableProvider";
 const TotalPot = () => {
   const { total } = useGameTable();
   const { getTranslatedWord } = useTranslation();
-
-  const gameClass = useSelector((state) => state.game.game);
-
-  // Définir currentStack avec une valeur par défaut de 0
-  const currentStack = gameClass?.game?.total || 0;
-
-  // Définir updatedStack à partir de currentStack
-  const [updatedStack, setUpdatedStack] = useState(currentStack);
-
-  useEffect(() => {
-    // Mettre à jour updatedStack si currentStack change
-    setUpdatedStack(currentStack);
-  }, [currentStack]);
 
   return (
     <div className={`container-playerPots`}>
