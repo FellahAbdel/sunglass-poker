eee7c414c7e57b0949f395e0936e094f5e066ea4
DEMANGEON Noé <noe.demangeon@gmail.com>
2024-05-12 14:06:07 +0200

    TextInput pour les password

--- app/web/src/components/Window/WindowContent/SpecificComponentWindow/ListTableItem.js
+++ app/web/src/components/Window/WindowContent/SpecificComponentWindow/ListTableItem.js
@@ -1,12 +1,24 @@
 // TableItem.js ou TableItem.jsx
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../../button/Button.tsx";
 import locked from "../../../assets/images/icons/white/password.png";
 import unlocked from "../../../assets/images/icons/white/unlock.png";
 import { useTranslation } from "../../../Utiles/Translations";
+import TextInputComponent from "../../../textInput/TextInput.jsx";
 
 const ListTableItem = ({ nom, rang, nombreDeJoueurs, ouvert, onJoinClick }) => {
   const { getTranslatedWord } = useTranslation();
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const handleJoinClick = () => {
+    if (!ouvert && password.trim() === "") {
+      setPasswordError("Please enter the password"); // Assurez-vous d'entrer un mot de passe
+      return;
+    }
+    setPasswordError(""); // Réinitialiser l'erreur si le mot de passe est présent
+    onJoinClick(password);
+  };
   return (
     <div className="tableRow">
       <div className="rowItem">{nom}</div>
@@ -21,11 +33,23 @@ const ListTableItem = ({ nom, rang, nombreDeJoueurs, ouvert, onJoinClick }) => {
       </div>
       <div className="rowItem">
         {nombreDeJoueurs < 10 ? (
-          <Button
-            label={getTranslatedWord("serverPanel.join")}
-            styleClass="btn-list_table btn-list_table_join back-color2"
-            onClick={onJoinClick}
-          />
+          <>
+            
+            <Button
+              label={getTranslatedWord("serverPanel.join")}
+              styleClass="btn-list_table btn-list_table_join back-color2"
+              onClick={handleJoinClick}
+            />
+            {!ouvert && (
+              <TextInputComponent
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                errorMessage={passwordError}
+                styleClass="input-password"
+              />
+            )}
+          </>
         ) : (
           <Button
             label={getTranslatedWord("serverPanel.full")}
