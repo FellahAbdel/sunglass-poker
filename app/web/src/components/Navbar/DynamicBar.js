e80e0121220f470033690b1cc8c8d9444c13b344
DEMANGEON Noé <noe.demangeon@gmail.com>
2024-05-12 20:13:02 +0200

    Servername gametable dynamic (il faut encore que le back l'envoi)

--- app/web/src/components/Navbar/DynamicBar.js
+++ app/web/src/components/Navbar/DynamicBar.js
@@ -1,52 +1,69 @@
-import React  from "react";
-import "./dynamicBar.css"
+import React from "react";
+import "./dynamicBar.css";
 import { useWindowContext } from "../Utiles/WindowContext";
 import { formatNumber } from "./../Utiles/NumberFormat";
 import Button from "./../button/Button.tsx";
 import { useUserData } from "../Utiles/useUserData.jsx";
 import { useTranslation } from "./../Utiles/Translations.jsx";
+import { useGameTable } from "../Utiles/GameTableProvider.jsx";
 
-const DynamicBar = () =>{
-    const { openWindow ,windowType , isGameTableVisible } =
-    useWindowContext();
-    const { user } = useUserData();
-    const { getTranslatedWord } = useTranslation();
+const DynamicBar = () => {
+  const { openWindow, windowType, isGameTableVisible } = useWindowContext();
+  const { user } = useUserData();
+  const { getTranslatedWord } = useTranslation();
+  const { currentGame } = useGameTable();
 
+  return (
+    <>
+      {/* User coins */}
+      <div
+        className={`container-userCoins 
+                            ${
+                              (windowType === "shop" ||
+                                windowType === "coins") &&
+                              "appear"
+                            }
+                            ${windowType === "coins" && "center"}`}
+      >
+        <div className="userCoinsTop">{formatNumber(user.coins)} SC</div>
+        {(windowType === "shop" || windowType === "coins") && (
+          <Button
+            label={
+              windowType === "shop"
+                ? getTranslatedWord("shop.buyMore")
+                : getTranslatedWord("shop.backStore")
+            }
+            styleClass={`btn-coinsShop`}
+            onClick={
+              windowType === "shop"
+                ? () => openWindow("coins")
+                : () => openWindow("shop")
+            }
+          />
+        )}
+      </div>
 
-    return (
-        <>
-            {/* User coins */}
-            <div className={`container-userCoins 
-                            ${(windowType === "shop" || windowType === "coins") && "appear"}
-                            ${(windowType === "coins") && "center"}`}
-                        >
-            <div className="userCoinsTop">
-            {formatNumber(user.coins)} SC
-            </div>
-            {(windowType === "shop" || windowType === "coins") &&
-                <Button
-                    label={windowType === "shop" ? getTranslatedWord("shop.buyMore") : getTranslatedWord("shop.backStore")}
-                    styleClass={`btn-coinsShop`}
-                    onClick={windowType === "shop" ? () => openWindow("coins") : () => openWindow("shop")}
-                />}
-            </div>
+      {/* Create a game button */}
+      <div
+        className={`container-userCoins 
+                            ${windowType === "servers" && "appear"}`}
+      >
+        <Button
+          label={getTranslatedWord("game.createAgame")}
+          styleClass={`btn-coinsShop btn-pnl-createAgame`}
+          onClick={() => openWindow("create_table")}
+        />
+      </div>
 
-            {/* Create a game button */}
-            <div className={`container-userCoins 
-                            ${(windowType === "servers") && "appear"}`}
-                        >
-                <Button
-                    label={getTranslatedWord("game.createAgame")}
-                    styleClass={`btn-coinsShop btn-pnl-createAgame`}
-                    onClick={() => openWindow("create_table")}
-                />     
-            </div>
-
-            <div className={`container-serverInfo back-color1 ${(isGameTableVisible) && "appear"}`}>
-                Server Name
-            </div>
-        </>
-    )
-}
+      <div
+        className={`container-serverInfo back-color1 ${
+          isGameTableVisible && "appear"
+        }`}
+      >
+        {currentGame ? currentGame.serverName : "No Active Server"}
+      </div>
+    </>
+  );
+};
 
 export default DynamicBar;
