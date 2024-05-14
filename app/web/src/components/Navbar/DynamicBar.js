27f47672260c5cf29d25f107a7c867ac4c4d5f14
Mostafahqv Mac <mostafa.haghvirdiloo@etu.unistra.fr>
2024-05-14 18:51:47 +0200

    start the game button placement change

--- app/web/src/components/Navbar/DynamicBar.js
+++ app/web/src/components/Navbar/DynamicBar.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./dynamicBar.css";
+import { useAuth } from "./../Utiles/AuthProvider.jsx";
 import { useWindowContext } from "../Utiles/WindowContext";
 import { formatNumber } from "./../Utiles/NumberFormat";
 import Button from "./../button/Button.tsx";
 import { useUserData } from "../Utiles/useUserData.jsx";
 import { useTranslation } from "./../Utiles/Translations.jsx";
 import { useGameTable } from "../Utiles/GameTableProvider.jsx";
+import * as actions from "../../store/actions/clientInteractionsCreator.js";
+import { useDispatch } from "react-redux";
+
 
 const DynamicBar = () => {
+  const { userId } = useAuth();
   const { openWindow, windowType, isGameTableVisible } = useWindowContext();
+  const { isMaster, showWaitingMessage , isSpectator } = useGameTable();
   const { user } = useUserData();
   const { getTranslatedWord } = useTranslation();
   const { serverName } = useGameTable();
+  const dispatch = useDispatch();
+
+  const startGame = () => {
+    // Logique pour commencer la partie
+    console.log("Starting game with roomId:");
+    dispatch(actions.startGame(userId));
+  };
+
+  useEffect(() => {
+    console.log("showWaitingMessage:", showWaitingMessage);
+  }, [showWaitingMessage]);
 
   return (
     <>
@@ -63,6 +80,32 @@ const DynamicBar = () => {
       >
         {serverName ? serverName : getTranslatedWord("game.noActiveServer")}
       </div>
+
+      {/* Waiting panel and start button */}
+      {isGameTableVisible && 
+        <div className={`container-waiting ${showWaitingMessage && "appear"}`}>
+          <div className="txt-waiting">
+            {getTranslatedWord("table.waiting")}
+          </div>
+          {isMaster ? (
+            <Button
+              styleClass="btn-gameStart2 back-color1"
+              label={getTranslatedWord("table.start")}
+              onClick={() => startGame()}
+            />
+          ) : (
+            <Button
+              styleClass="btn-gameStart2 back-color1"
+              label={
+                isSpectator
+                  ? getTranslatedWord("table.join")
+                  : getTranslatedWord("table.spectacle")
+              }
+              onClick={() => startGame()}
+            />
+          )}
+        </div>
+      }
     </>
   );
 };
