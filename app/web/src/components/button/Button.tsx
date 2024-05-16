d72e2accf584008613148962457d23ee05ef2015
Mostafahqv Mac <mostafa.haghvirdiloo@etu.unistra.fr>
2024-05-16 22:46:41 +0200

    new GameActionPanel

--- app/web/src/components/button/Button.tsx
+++ app/web/src/components/button/Button.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./button.css";
 
-const Button = ({ label, iconSrc, styleClass, onClick }) => {
+const Button = ({ label, iconSrc, styleClass, onClick , disabled}) => {
   const handleClick = (event) => {
     event.stopPropagation(); // Ajout pour stopper la propagation
     onClick && onClick(event); // Appelle la fonction onClick passée en props si elle existe
   };
   return (
-    <button className={styleClass} onClick={handleClick}>
+    <button className={styleClass} onClick={handleClick} disabled={disabled}>
       <span>{label}</span>
       {iconSrc && <img src={iconSrc} alt={label}/>}
     </button>
