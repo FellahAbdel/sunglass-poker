bb6ce538b9e0d70ca2b50870c30cb86a5680e2a4
fellah <abdoulazizdiallofouta@gmail.com>
2024-05-16 14:39:22 +0200

    Add documentation for game.js

--- app/server/controller/game.js
+++ app/server/controller/game.js
@@ -2,6 +2,13 @@ const texasHoldem = require('./rules');
 const Player = require("../shared/Player");
 const actions = require('../store/actions/actionTypes');
 
+/**
+ * Initialize a game room
+ * @param {string} id - The id of the game room
+ * @param {Array<Player>} players - The players in the game room
+ * @param {Object} rules - The rules of the game
+ * @returns {Object} - The game room
+ */
 module.exports = initGameRoom = function (id,players = [],rules = texasHoldem) {
     return {
         id: id,
