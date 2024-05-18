76b616697f2e02d59edefea2031c20b60ad0cede
fellah <abdoulazizdiallofouta@gmail.com>
2024-05-18 16:42:13 +0200

    Even though we received the plyer sit sometimes it took time interface to be refresh, that's why I add a timeout

--- app/web/src/components/Window/WindowContent/LoadingWindow.js
+++ app/web/src/components/Window/WindowContent/LoadingWindow.js
@@ -3,24 +3,23 @@ import { useSelector } from "react-redux";
 import { useWindowContext } from "../../Utiles/WindowContext.jsx";
 
 const LoadingWindow = () => {
-    const { closeWindow, showGameTable, setWindowType } = useWindowContext();
-    const isPlayerSited = useSelector((state) => state.game.playerSited);
+  const { closeWindow, showGameTable, setWindowType } = useWindowContext();
+  const isPlayerSited = useSelector((state) => state.game.playerSited);
 
-    //for server joining from ServerPanel
-    useEffect(() => {
-        if (isPlayerSited) {
-            console.log("Game room displayed!");
-            showGameTable();
-            closeWindow();
-            setWindowType("");
-            console.log("playerSited", isPlayerSited);
-        }
-    }, [isPlayerSited]);
+  //for server joining from ServerPanel
+  useEffect(() => {
+    if (isPlayerSited) {
+      setTimeout(function () {
+        console.log("Game room displayed!");
+        showGameTable();
+        closeWindow();
+        setWindowType("");
+        console.log("playerSited", isPlayerSited);
+      }, 100);
+    }
+  }, [isPlayerSited]);
 
-  return (
-    <div className="main-loadingWindow">
-    </div>
-  );
+  return <div className="main-loadingWindow"></div>;
 };
 
 export default LoadingWindow;
