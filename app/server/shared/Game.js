514f3bc0693b49da98fbccccd3ba3a08606fb02f
Victor-Gierlowski <59644630+Victor-Gierlowski@users.noreply.github.com>
2024-05-19 22:48:11 +0200

    firsttotalk

--- app/server/shared/Game.js
+++ app/server/shared/Game.js
@@ -38,6 +38,7 @@ class Game {
       allow_start: true,
       serverName: "",
       firstRoundForRoom: true,
+      firstToTalk:0,
     };
     Object.assign(this, basedValue, ...args);
     // this.activePlayers = null;
@@ -369,6 +370,20 @@ class Game {
         p.currentBetTurn = 0;
       });
       this.gameCurrentBet = 0;
+      this.focus = this.firstToTalk;
+      while (
+        !this.players[this.focus].isActive ||
+        this.players[this.focus].getStatus() === "tapis"
+      ) {
+        this.focus = (this.focus + 1) % this.players.length;
+        if (this.focus === originalFocus) {
+          clearTimeout(this.focusTurnCall);
+          while (this.currentStage !== "showdown") {
+            this.advanceStage();
+          }
+          return;
+        }
+      }
       this.advanceStage();
       // return;
     }
@@ -837,8 +852,9 @@ class Game {
     this.currentStage = "preflop";
     this.state = "active";
     this.rotateStartingPlayer();
-    this.focus = this.startingPlayerIndex;
-    this.playerBeforeNextTurn = this.startingPlayerIndex;
+    this.firstToTalk = this.firstToTalk % this.players.length;
+    this.focus = this.firstToTalk;
+    this.firstToTalk++;
     this.total = 0;
     this.pokerTable.reset();
     this.deck = new Deck();
@@ -856,7 +872,7 @@ class Game {
     console.log("firstplayer: ", firstPlayer);
     firstPlayer.betinitial(this.gameCurrentBet / 2);
     this.total += this.gameCurrentBet / 2;
-
+    
     this.rotateFocus();
     const nextPlayer = this.players[this.focus];
     console.log("nextPlayer: ", nextPlayer);
@@ -864,8 +880,7 @@ class Game {
     this.total += this.gameCurrentBet;
 
     this.rotateFocus();
-    this.playerBeforeNextTurn = this.focus;
-
+    this.firstToTalk = this.focus;
     //OU ICI
 
     //IL VA SUREMENT MANQUE UN JOUEUR A CHECK AVANT D'AFFICHER LE FLOP
