5aef49bb97f1abc24041895f0b89397e0312dda9
Ziad <ziad.etu@unistra.fr>
2024-05-19 18:24:21 +0200

    recorrection

--- app/server/shared/score-engine/straight.js
+++ app/server/shared/score-engine/straight.js
@@ -1,32 +1,3 @@
-/*
- * ...
- */
-
-/*function isStraight(sevenCardsTab, refact = false) {
-
-  let tab = [...sevenCardsTab];
-  let hand = [];
-
-  tab.sort((a, b) => a.number - b.number);
-
-  while (tab.length !== 0) {
-    let tmp = tab.pop();
-
-    if (hand.length === 0) {
-      hand.push(tmp);
-    } else if (hand.length < 5) {
-      if (tmp.number === hand[hand.length - 1].number - 1) {
-        hand.push(tmp);
-      } else {
-        hand.splice(0, hand.length);
-        hand.push(tmp);
-      }
-    }
-  }
-
-  return hand.length === 5 ? hand : false;
-}*/
-
 function isStraight(sevenCardsTab) {
   let tab = [...sevenCardsTab];
   let hand = []; // Main pour accumuler les suites potentielles
